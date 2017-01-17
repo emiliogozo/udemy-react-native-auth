e939cbc0c736c91e706bd26c198d4b2c73e46db5
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-17 21:20:35 +0800

    chore: Firebase client setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBbFDhe6NKmd8pY_v2pTaxAs14okRK24l0',
+      authDomain: 'auth-3f979.firebaseapp.com',
+      databaseURL: 'https://auth-3f979.firebaseio.com',
+      storageBucket: 'auth-3f979.appspot.com',
+      messagingSenderId: '471208617348'
+    });
+  }
   render() {
     return (
       <View>
